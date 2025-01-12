a4225b69ed24cc2b04f0522f6886505c3deee5ab
Ammar-Ali234 <mammarali299@gmail.com>
2025-01-12 09:24:38 +0500

    with Webcam script

--- main_with_video.py
+++ main_with_video.py
@@ -3,8 +3,10 @@ import cv2
 import time
 
 def detect_guns_from_webcam():
-    model = YOLO('Gun-Detection\\best.pt')  
-    cap = cv2.VideoCapture(0)  
+    model = YOLO('C:\\Users\\HP\\OneDrive\\Desktop\\Portfolio\\Gun\\best.pt')  
+    gun_class_id = 0  
+    
+    cap = cv2.VideoCapture(0)  # Access the webcam
     if not cap.isOpened():
         print("Error: Could not access webcam.")
         return
@@ -16,15 +18,17 @@ def detect_guns_from_webcam():
             break
         
         start_time = time.time()
-        results = model(frame, conf=0.65)[0]
+        results = model(frame, conf=0.4)[0]
         
         for box in results.boxes:
-            confidence = box.conf.item()
-            x1, y1, x2, y2 = box.xyxy[0].tolist()
-            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
-            conf_text = f'Gun: {confidence:.2f}'
-            cv2.putText(frame, conf_text, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+            class_id = int(box.cls.item())  
+            if class_id == gun_class_id:  
+                confidence = box.conf.item()
+                x1, y1, x2, y2 = box.xyxy[0].tolist()
+                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
+                conf_text = f'Gun: {confidence:.2f}'
+                cv2.putText(frame, conf_text, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
         
         fps = 1 / (time.time() - start_time)
         fps_text = f'FPS: {fps:.2f}'
